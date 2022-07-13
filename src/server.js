3fb87f124029746ced4838cd6d6ffe9002204d00
Juan Barezzi <juan.barezzi@gmail.com>
2022-07-12 21:18:05 -0300

    feat: add routers to server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import router from "./routes";
 
 const app = express();
-app.use(cors(), express.json());
 dotenv.config();
+
+app.use(cors(), express.json(), router);
 
 app.listen(process.env.PORT);
