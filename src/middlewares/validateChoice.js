0bba0e722e90272d0288c807a8c903baa5cd14c6
Juan Barezzi <juan.barezzi@gmail.com>
2022-07-13 23:40:02 -0300

    feat: implement choice validation

---
+++ src/middlewares/validateChoice.js
@@ -0,0 +1,19 @@
+import { db, objectId } from "./../database/mongo.js";
+import { choiceSchema } from "./schemas/index.js";
+
+async function validateChoice(req, res, next) {
+    const { title, poolId } = req.body;
+    try {
+        await choiceSchema.validateAsync({ title, poolId });
+        const titleExist = await db.collection("choices").findOne({ poolId: objectId(poolId), title: title });
+        if(titleExist) {
+            res.sendStatus(409);
+        }
+        next();
+    } catch(error) {
+        const errors = error.details.map(detail => detail.message);
+        res.status(422).send(errors);
+    }   
+}
+
+export default validateChoice;
