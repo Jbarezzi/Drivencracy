6d524debb12cc3d3501787f5d05a1e3f02be3587
Juan Barezzi <juan.barezzi@gmail.com>
2022-07-14 19:27:13 -0300

    feat: implement polls choices get

--- src/controllers/pollController.js
+++ src/controllers/pollController.js
@@ -1,5 +1,5 @@
 import dayjs from "dayjs";
-import { db } from "./../database/mongo.js";
+import { db, objectId } from "./../database/mongo.js";
 
 async function createPoll(req, res) {
     const poll = req.body;
@@ -19,8 +19,18 @@ async function getPolls(_req, res) {
         const polls = await db.collection("polls").find().toArray();
         res.send(polls);
     } catch {
-        res.send(503);
+        res.sendStatus(503);
     }
 }
 
-export { createPoll, getPolls };
+async function getPollChoices(req, res) {
+    const id = req.params.id;
+    try {
+        const choices = await db.collection("choices").findOne({ poolId: id }).toArray();
+        res.send(choices);
+    } catch {
+        res.sendStatus(503);
+    }
+}
+
+export { createPoll, getPolls, getPollChoices };
