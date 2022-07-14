8a6e73698062e7b304231dfe414fd57db7f9f153
Juan Barezzi <juan.barezzi@gmail.com>
2022-07-14 19:27:47 -0300

    feat: add validation for polls existence

---
+++ src/middlewares/checkIfPollExists.js
@@ -0,0 +1,16 @@
+import { db, objectId } from "./../database/mongo.js";
+
+async function checkIfPollExists(req, res, next) {
+    const id = req.params.id;
+    try {
+        const isPollValid = await db.collection("polls").findOne({ _id: objectId(id) });
+        if(!!isPollValid) {
+            next();
+        }
+        res.sendStatus(404);
+    } catch{
+        res.sendStatus(503)
+    }
+}
+
+export default checkIfPollExists;
